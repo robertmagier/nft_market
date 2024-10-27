585d5007619e9a082e97ae0a22449d2855801545
Robert Magier <robert.magier@gmail.com>
2024-10-27 08:17:35 +0100

    Add dummy USDC smart contract

---
+++ smart_contracts/contracts/dummies/USDC.sol
@@ -0,0 +1,15 @@
+// contracts/Nft.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract DummyUSDC is ERC20('USDC', 'USDC') {
+  constructor() {
+    _mint(msg.sender, 10 * 10 ** decimals());
+  }
+
+  function decimals() public pure override returns (uint8) {
+    return 6;
+  }
+}
