2c20b68a6c178f0eea763bb162fcaad6066bfe0d
Robert Magier <robert.magier@gmail.com>
2024-11-08 18:46:10 +0100

    Add custom security contract

--- smart_contracts/contracts/dummies/USDT.sol
+++ smart_contracts/contracts/dummies/USDT.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.27;
 
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
+/// @custom:security-contact N/A
 contract DummyUSDT is ERC20('USDC', 'USDC') {
   constructor() {
     _mint(msg.sender, 10 * 10 ** decimals());
