4ef25d356bcf0dc62dbc7d0a18b73e43d8e778c4
Robert Magier <robert.magier@gmail.com>
2024-11-09 17:47:40 +0100

    Add natspec comments to dummies

--- smart_contracts/contracts/dummies/DummyWrongUpgradedNft.sol
+++ smart_contracts/contracts/dummies/DummyWrongUpgradedNft.sol
@@ -16,8 +16,21 @@ error FeeCollectionFailed(address token, address owner, uint256 feeAmount);
 error FeeWithdrawalFailed(address token, address owner, uint256 balance);
 
 /// @custom:security-contact N/A
-contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeable {
+/// @title NFT Contract
+/// @notice This contract allows for the minting, buying, and transferring of NFTs with USDT payment and fee handling.
+contract Nft is ERC721URIStorageUpgradeable, OwnableUpgradeable {
+  /// @notice Emitted when a new token is created.
+  /// @param tokenId The ID of the newly created token.
+  /// @param tokenURI The URI of the token's metadata.
+  /// @param price The price of the token in USDT.
+  /// @param owner The owner of the token.
   event TokenCreated(uint256 indexed tokenId, string tokenURI, uint256 price, address indexed owner);
+
+  /// @notice Emitted when a token is bought.
+  /// @param tokenId The ID of the token being bought.
+  /// @param price The price paid for the token in USDT.
+  /// @param seller The address of the seller.
+  /// @param buyer The address of the buyer.
   event TokenBought(uint256 indexed tokenId, uint256 price, address indexed seller, address indexed buyer);
 
   struct TokenConfig {
@@ -25,12 +38,22 @@ contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeabl
     address owner;
   }
 
+  /// @notice Address of the USDT token contract.
   address public USDTTokenAddress;
+
+  /// @notice The percentage fee collected on each transaction.
   uint256 public feePercentage;
+
+  /// @notice Tracks the ID for the next token to be minted.
   uint256 private _tokenId;
-  uint256 public incorrectStorageChange;
+
+  /// @notice Variable added to test the upgradeability of the contract. It should fail the upgrade. New variable can be added only in the end of the storage.
+  uint256 public wrongMissmatchStorage;
+
+  /// @notice Default percentage increase applied to the token price after each sale.
   uint256 private _defaultPriceIncreasePer;
 
+  /// @notice Configuration mapping for each token.
   mapping(uint256 tokenId => TokenConfig config) public tokenConfig;
 
   modifier onlyTokenOwner(uint256 tokenId) {
@@ -58,10 +81,13 @@ contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeabl
   }
 
   /// @custom:oz-upgrades-unsafe-allow constructor
+  /// @notice Disables initializer functions for the contract.
   constructor() {
     _disableInitializers();
   }
 
+  /// @notice Initializes the NFT contract with the specified USDT address and initial settings.
+  /// @param USDT The address of the USDT token contract.
   function initialize(address USDT) external initializer {
     USDTTokenAddress = USDT;
     __ERC721_init('NFT', 'NFT');
@@ -71,6 +97,10 @@ contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeabl
     _defaultPriceIncreasePer = 10;
   }
 
+  /// @notice Creates a new NFT with a specified URI and price.
+  /// @param tokenURI The URI for the token's metadata.
+  /// @param price The initial price of the token in USDT.
+  /// @return The ID of the newly created token.
   function create(string memory tokenURI, uint256 price) external returns (uint256) {
     if (bytes(tokenURI).length == 0) {
       revert EmptyURI();
@@ -85,18 +115,28 @@ contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeabl
     return newId;
   }
 
+  /// @notice Updates the fee percentage for transactions.
+  /// @param newFeePercentage The new fee percentage.
   function setFeePercentage(uint256 newFeePercentage) external onlyOwner {
     feePercentage = newFeePercentage;
   }
 
+  /// @notice Calculates the fee for a specified token.
+  /// @param tokenId The ID of the token.
+  /// @return The fee amount in USDT.
   function expectedFee(uint256 tokenId) external view returns (uint256) {
     return _expectedFee(tokenId);
   }
 
+  /// @notice Calculates the total cost, including fees, to buy a specified token.
+  /// @param tokenId The ID of the token.
+  /// @return The total cost in USDT.
   function expectedBuyCost(uint256 tokenId) external view returns (uint256) {
     return tokenConfig[tokenId].price + _expectedFee(tokenId);
   }
 
+  /// @notice Allows a user to buy a specified token.
+  /// @param tokenId The ID of the token to buy.
   function buy(uint256 tokenId) external notTokenOwner(tokenId) tokenExists(tokenId) {
     if (tokenConfig[tokenId].price == 0) {
       revert TokenNotForSale(tokenId);
@@ -113,48 +153,68 @@ contract DummyWrongUpgradedNft is ERC721URIStorageUpgradeable, OwnableUpgradeabl
     _increasePrice(tokenId);
   }
 
+  /// @notice Sets a new price for a specified token.
+  /// @param tokenId The ID of the token.
+  /// @param price The new price in USDT.
   function setPrice(uint256 tokenId, uint256 price) external onlyTokenOwner(tokenId) {
     tokenConfig[tokenId].price = price;
   }
 
+  /// @notice Transfers a specified token to another address.
+  /// @param to The address to transfer the token to.
+  /// @param tokenId The ID of the token to transfer.
   function transfer(address to, uint256 tokenId) external onlyTokenOwner(tokenId) {
     _transfer(msg.sender, to, tokenId);
     tokenConfig[tokenId].owner = to;
   }
 
+  /// @notice Allows the contract owner to withdraw collected fees in USDT.
   function withdrawFees() external onlyOwner {
     if (!IERC20(USDTTokenAddress).transfer(msg.sender, IERC20(USDTTokenAddress).balanceOf(address(this)))) {
       revert FeeWithdrawalFailed(USDTTokenAddress, msg.sender, IERC20(USDTTokenAddress).balanceOf(address(this)));
     }
   }
 
+  /// @dev Increases the price of a token after a purchase by a default percentage.
+  /// @param tokenId The ID of the token.
   function _increasePrice(uint256 tokenId) private {
     uint256 newPrice = tokenConfig[tokenId].price + (tokenConfig[tokenId].price * _defaultPriceIncreasePer) / 100;
     tokenConfig[tokenId].price = newPrice;
   }
 
+  /// @dev Checks if a token with the specified ID exists.
+  /// @param tokenId The ID of the token to check.
+  /// @return True if the token exists, false otherwise.
   function _exists(uint256 tokenId) private view returns (bool) {
     return ownerOf(tokenId) != address(0);
   }
 
+  /// @dev Collects payment for a token transfer.
+  /// @param tokenId The ID of the token.
   function _collectPayment(uint256 tokenId) private {
     if (!IERC20(USDTTokenAddress).transferFrom(msg.sender, tokenConfig[tokenId].owner, tokenConfig[tokenId].price)) {
       revert PaymentCollectionFailed(USDTTokenAddress, tokenConfig[tokenId].owner, tokenConfig[tokenId].price);
     }
   }
 
+  /// @dev Calculates the fee for a specified token based on its price and fee percentage.
+  /// @param tokenId The ID of the token.
+  /// @return The calculated fee in USDT.
   function _expectedFee(uint256 tokenId) private view returns (uint256) {
     return (tokenConfig[tokenId].price * feePercentage) / 100;
   }
 
+  /// @dev Collects transaction fees for a token sale.
+  /// @param fee The fee amount to collect in USDT.
   function _collectFees(uint256 fee) private {
     if (!IERC20(USDTTokenAddress).transferFrom(msg.sender, address(this), fee)) {
       revert FeeCollectionFailed(USDTTokenAddress, address(this), fee);
     }
   }
 
+  /// @dev Function added to test upgradeability.
   function newFunction() external returns (string memory) {
-    incorrectStorageChange = 1;
+    wrongMissmatchStorage++;
     return 'new function';
   }
 }
